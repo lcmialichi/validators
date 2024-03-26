4d2a14232d863118583ae3386bbcbe658e8632e1
LUCAS MIALICHI DA COSTA <LMCOSTA@trf3.jus.br>
2024-03-26 15:09:02 -0300

    fix contract

--- src/Handlers/Cep.php
+++ src/Handlers/Cep.php
@@ -5,7 +5,7 @@ namespace Validators\Handlers;
 
 class Cep implements \Validators\Contracts\ValidatorHandler
 {
-    public function handle($item)
+    public function handle($item): bool
     {
         return preg_match("/[0-9]{5}[\d]{3}/", str_replace("-", "", $item));
     }
--- src/Handlers/IntReserved_.php
+++ src/Handlers/IntReserved_.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Validators\Handlers;
 
 class IntReserved_ implements \Validators\Contracts\ValidatorHandler
